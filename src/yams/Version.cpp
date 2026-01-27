c723208b01bb091720ecadbe8e893b948dc725bb
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2026-01-27 21:52:19 +0100

    Adds yams::GetVersion() function

---
+++ src/yams/Version.cpp
@@ -0,0 +1,12 @@
+#include "Version.hpp"
+#include "git.h"
+
+namespace yams {
+std::string GetVersion() {
+	if (git::AnyUncommittedChanges()) {
+		return std::string{git::Describe()} + "-dirty";
+	} else {
+		return std::string{git::Describe()};
+	}
+}
+} // namespace yams
---
+++ src/yams/Version.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+namespace yams {
+
+std::string GetVersion();
+
+}
